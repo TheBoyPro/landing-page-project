b1220cd24db5e6dbbd61c173e9e06aa8c1a879c3
TheBoyPro <129667590+TheBoyPro@users.noreply.github.com>
2023-07-15 07:34:38 +0100

    index.js

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ window.intlTelInput(input,{})
 
 var input_footer = document.querySelector('#phone-footer')
 window.intlTelInput(input_footer,{})
+
+const primaryHeader = document.querySelector('.navbar')
+const scrollWatcher = document.createElement('div')
+
+scrollWatcher.setAttribute('data-scroll-watcher', '')
+primaryHeader.before(scrollWatcher)
+
+const navObserver = new IntersectionObserver((entries) => {
+    primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting)
+})
+
+navObserver.observe(scrollWatcher)
